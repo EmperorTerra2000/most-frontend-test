f85a64000d7018e8f11d8052d6a5120bccb614df
Trofim Abramov <EmperorTerra2000@yandex.ru>
2023-12-07 23:13:13 +0900

    update: add Link element/ change css style

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,31 @@
+import { Link } from 'react-router-dom';
+
 import './Header.css';
 
 function Header({ isOpen, onClose, loggedIn }) {
+  const styleLink = {
+    display: 'block',
+    textDecoration: 'none',
+    color: 'inherit',
+  };
+
   return (
     <header className="header">
       <div className="header__content">
-        <p className="header__title font-large">ONLINE STORE</p>
+        <p className="header__title font-large">
+          <Link to="/" style={styleLink}>
+            ONLINE STORE
+          </Link>
+        </p>
         <div className="header__nav-block">
           {loggedIn ? (
-            <p className="header__nav font-medium">Sign in</p>
+            <Link to="/signin" style={styleLink}>
+              <p className="header__nav font-medium">Sign in</p>
+            </Link>
           ) : (
-            <p className="header__nav font-medium">Profile</p>
+            <Link to="/profile" style={styleLink}>
+              <p className="header__nav font-medium">Profile</p>
+            </Link>
           )}
         </div>
       </div>
